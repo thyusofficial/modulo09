f193ab8653edc94f81b9282e827bdd92806b3ad5
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2020-02-05 14:28:15 -0300

    auth loading, toasts, signup *unfinished

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('O nome é obrigatório'),
@@ -15,11 +18,13 @@ const schema = Yup.object().shape({
     .required('A senha é obrigatória'),
 });
 
-function handleSubmit(data) {
-  console.tron.log(data);
-}
-
 export default function Signup() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
