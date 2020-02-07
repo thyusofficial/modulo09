522ac1bfcd79ed2b125188df223c10104acc39a5
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2020-02-07 16:45:46 -0300

    dashboard style, days navigation, list schedule *unfinished*

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -25,12 +25,14 @@ export default function Header() {
               <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
+
             <img
               src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/50/abott@adorable.png'
+                profile.avatar
+                  ? profile.avatar.url
+                  : 'https://api.adorable.io/avatars/50/abott@adorable.png'
               }
-              alt="Matheus Cardoso"
+              alt="Avatar"
             />
           </Profile>
         </aside>
